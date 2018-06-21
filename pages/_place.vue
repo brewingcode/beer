c5d45386726c988aa8efb0edd4a8317b73359b12
Alex <alex@brewingcode.net>
2018-06-21 15:46:07 -0700

    Move beer list into a dynamic route

---
+++ pages/_place.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+div
+  template(v-for='v,k in places')
+    nuxt-link(:to='k') {{k}}
+  beer-list(:getBeers="getBeers")
+</template>
+
+<script lang="coffee">
+import yardhouse from '~/lib/yardhouse.coffee'
+import pinebox from '~/lib/pinebox.coffee'
+
+places = {
+  yardhouse
+  pinebox
+}
+
+export default
+  data: ->
+    places: places
+  computed:
+    getBeers: ->
+      places[@$route.params.place]
+  validate: ({params}) ->
+    places[params.place]
+  head: ->
+    title: 'Beer: ' + @$route.params.place
+</script>
