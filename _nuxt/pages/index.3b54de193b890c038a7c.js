f17d144d2873fcd99e8a0ac1029b6b5c5db064f1
Alex <alex@brewingcode.net>
2018-06-21 21:58:31 -0700

    Generate gh-pages for adf51c2

---
+++ _nuxt/pages/index.3b54de193b890c038a7c.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{"/TYz":function(e,l,n){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var u=n("VU/8")(null,null,!1,null,null,null);u.options.__file="pages/index.vue",l.default=u.exports}});
